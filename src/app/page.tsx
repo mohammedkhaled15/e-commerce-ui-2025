f7803a354caf4671b55eff028c8abf78d61b54fd
Mohammedkhaled15 <mohammedkhaled15@gmail.com>
2025-11-22 15:05:56 +0200

    categories and product card and list and filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import ProductsList from "@/components/ProductsList"
 import Image from "next/image"
 
-const Homepage = () => {
+const Homepage = async ({searchParams}:{searchParams:Promise<{category:string}>}) => {
+
+  const category = (await searchParams).category
   return (
     <div className=''>
       <div className="relative aspect-[3/1] mb-12">
         <Image src="/featured.png" alt="Featured Products" fill />
       </div>
+      <ProductsList category={category} params={"homepage"}/>
     </div>
   )
 }
